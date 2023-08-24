817b9aab09b9eee220f82198da7d3fb3a09da692
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-08-24 23:48:58 +0530

    Area of Triangle

---
+++ 2_arearOfTrianle.js
@@ -0,0 +1,6 @@
+let s1=5;
+let s2=6;
+let s3=7;
+let perimeter=(s1+s2+s3)/2;
+let area=Math.sqrt(perimeter*(s1-perimeter)*(s2-perimeter)*(s3-perimeter));
+console.log(`Area of triangle is ${area}`);
