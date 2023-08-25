615763bd6bf13f16af1e040e5627fe5d779d111a
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-08-25 21:12:29 +0530

    Calculate Sundays From the month of 2014 to 2050 has been solved

---
+++ 4_CalculateSundays.js
@@ -0,0 +1,9 @@
+// Write a JavaScript program to find out if 1st January will be a Sunday between 2014 and 2050.  
+for(let year=2014;year<=2050;year++)
+{
+    let day=new Date(year,0,1);
+    if(day.getDay()===0)
+    {
+        console.log(`1st January is Sunday in ${year}`);
+    }
+}
