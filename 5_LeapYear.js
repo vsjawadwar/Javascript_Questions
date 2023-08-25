bcb3684041630b49e961847b45dfe1cded896057
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-08-26 02:13:48 +0530

    Leap year problem solved by using user-defined function

---
+++ 5_LeapYear.js
@@ -0,0 +1,7 @@
+function leapYear(year)
+{
+    return (year % 100 === 0)?(year % 400 === 0):(year % 4 === 0);
+}
+console.log(leapYear(2016));
+console.log(leapYear(2000));
+console.log(leapYear(1700));
