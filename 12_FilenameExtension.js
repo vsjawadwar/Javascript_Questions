f4382b9fb3aedaeb31c69b3993c097675ef63e58
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-08-29 03:50:55 +0530

    File extension string problem has been solved

---
+++ 12_FilenameExtension.js
@@ -0,0 +1,7 @@
+// Write a JavaScript exercise to get the filename extension.
+let str1='Vishal.java';
+let str2='Vishal.c';
+let str3='Vishal.exe';
+console.log(str1.split('.').pop());
+console.log(str2.split('.').pop());
+console.log(str3.split('.').pop());
