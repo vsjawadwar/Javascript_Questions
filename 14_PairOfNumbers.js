3f4ef73da6f23df90663a33a1c841b17a63968c9
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-08-29 04:18:08 +0530

    Pair of numbers problem has been solved (1)

--- 14_PairOfNumbers.js
+++ 14_PairOfNumbers.js
@@ -1,12 +1,15 @@
 // Write a JavaScript program to check a pair of numbers and return true if one of the numbers is 50 or if their sum is 50.
+// function compare(a,b){
+//     if(a === 50 || b===50){
+//         return true;
+//     }
+//     else if(a+b===50){
+//         return true;
+//     }
+//     return false;
+// }
 function compare(a,b){
-    if(a === 50 || b===50){
-        return true;
-    }
-    else if(a+b===50){
-        return true;
-    }
-    return false;
+    return ((a==50 || b==50) || (a+b==50));
 }
 console.log(compare(50,2));
 console.log(compare(0,2));
