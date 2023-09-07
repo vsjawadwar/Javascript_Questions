65ffc31c7a17e0c3aa558f8cfbac795869e8cea0
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-09-07 23:39:50 +0530

    Computation of two numbers and evaluaton it in given conditios has been solved

---
+++ Basic_1/40_NumCheck.js
@@ -0,0 +1,21 @@
+// Write a JavaScript program to check from two given integers whether one of them is 8 or their sum or difference is 8.
+function numCheck(a,b){
+    if(a===8 || b === 8){
+        return "There is 8 in either a or b";
+    }
+    else if((a + b) === 8){
+        return "Sum is 8";
+    }else if((a-b)===8){
+        return "Substraction is 8";
+    }else{
+        return "Every condition fails";
+    }
+}
+console.log(numCheck(8,10));
+console.log(numCheck(10,8));
+console.log(numCheck(4,4));
+console.log(numCheck(24,16));
+console.log(numCheck(16,16));
+
+
+
