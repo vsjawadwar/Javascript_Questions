55f42e12dd6838b0fa79d68070c7cfec9fe868b9
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-09-09 04:54:27 +0530

    String sort operation done by using split,sort and join method

---
+++ Basic_1/52_Sort.js
@@ -0,0 +1,6 @@
+// Write a JavaScript program to convert letters of a given string alphabetically.
+function sortString(str){
+    return str.split("").sort().join();
+}
+console.log(sortString("adbc"));
+
