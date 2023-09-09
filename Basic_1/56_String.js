9f56523b74e98929efdd01ffa63f29c6923b2047
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-09-10 03:22:05 +0530

    Program will divide two numbers and convert it into string

---
+++ Basic_1/56_String.js
@@ -0,0 +1,6 @@
+// Write a JavaScript program to divide two positive numbers and return the result as string with properly formatted commas.
+function substraction(a,b){
+    return Math.round(a/b).toString();
+}
+console.log(substraction(62,6));
+console.log(typeof(substraction(62,6)));
