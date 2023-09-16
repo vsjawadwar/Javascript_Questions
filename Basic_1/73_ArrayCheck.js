8524e745f391196317b23a2f277de183d9315d27
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-09-16 14:41:27 +0530

    Array has been reversed by using reverse function

---
+++ Basic_1/73_ArrayCheck.js
@@ -0,0 +1,5 @@
+// Write a JavaScript program to reverse the elements of a given array of integers of length 3.
+function arrayCheck(arr){
+return arr.reverse();
+}
+console.log(arrayCheck([1,2,3,4,5]));
