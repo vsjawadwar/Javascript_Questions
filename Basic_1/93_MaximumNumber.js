8fcf753f2660fc9e248523f9ec8e7986dd616672
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-09-22 23:20:19 +0530

    Maximum difference problem has been solved

--- Basic_1/93_MaximumNumber.js
+++ Basic_1/93_MaximumNumber.js
@@ -1,1 +1,13 @@
 // Write a JavaScript program to find the maximum difference among all possible pairs of a given array of integers.
+function maxDifference(arr){
+let difference=0;
+let result=0;
+for(let i=0;i<arr.length;i++){
+    for(let k=0;k!=i && k<arr.length;k++){
+        difference=Math.abs(arr[i]-arr[k]);
+        result=Math.max(result,difference);
+    }
+}
+return result;
+}
+console.log(maxDifference([1,2,3,4,5,7,8]));
