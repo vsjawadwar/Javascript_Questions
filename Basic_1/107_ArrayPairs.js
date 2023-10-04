c8061518fb07979e96d02fa13409ec8463180773
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-10-04 06:11:38 +0530

    Array pairs problem has been solved

---
+++ Basic_1/107_ArrayPairs.js
@@ -0,0 +1,19 @@
+/*
+Write a JavaScript program to find the number of sorted pairs formed by arrays of integers. This is such that one element in the pair is divisible by the other one.
+
+For example - The output of [1, 3, 2] ->2 - (1,3), (1,2).
+The output of [2, 4, 6] -> 2 - (2,4), (2,6)
+The output of [2, 4, 16] -> 3 - (2,4), (2,16), (4,16)*/
+function arrayPairs(arr){
+let result=0;
+    for(let i=0; i<arr.length; i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[i] % arr[j] === 0 || arr[j] % arr[i] === 0)
+            {
+                result++;
+            }
+        }
+    }
+    return result;
+}
+console.log(arrayPairs([2,4,16]));
