995459369d405d33a430841a5ea740e8ace461b4
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-10-09 00:34:49 +0530

    Prime number problem has been solved

--- Basic_1/109_PrimeNumbers.js
+++ Basic_1/109_PrimeNumbers.js
@@ -1,5 +1,6 @@
 // Write a JavaScript program to sort an array of all prime numbers between 1 and a given integer. 
 let i=j=0;
+let arr=[];
 for(i =2;i<20;i++){
     for(j=2;j<i;j++){
         if(i % j == 0){
@@ -7,6 +8,7 @@ for(i =2;i<20;i++){
         }
     }
     if(i == j){
-        console.log(i);
+        arr.push(i);
     }
 }
+console.log(arr.sort((a,b)=>{return a-b}));
