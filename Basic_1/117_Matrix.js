f0523b72424263d8b1bfe2595891988c2f8ac6b4
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-10-10 23:26:18 +0530

    Matrix problem initiated

---
+++ Basic_1/117_Matrix.js
@@ -0,0 +1,6 @@
+/*
+Write a JavaScript program to check whether a given matrix is an identity matrix. 
+Note: In linear algebra, the identity matrix, or sometimes ambiguously called a unit matrix, of size n is the n ? n square matrix with ones on the main diagonal and zeros elsewhere.
+[[1, 0, 0], [0, 1, 0], [0, 0, 1]] -> true
+[[1, 0, 0], [0, 1, 0], [1, 0, 1]] -> false
+*/
