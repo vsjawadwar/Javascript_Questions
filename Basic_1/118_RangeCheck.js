80eca4b0f698a12333c38242c40e3f8f3f86d36a
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-10-10 23:35:32 +0530

    Range check problem has been solved

---
+++ Basic_1/118_RangeCheck.js
@@ -0,0 +1,6 @@
+// Write a JavaScript program to check whether a given number is in a given range. 
+function rangeCheck(a,b,c){
+return a<b && b<c ? true : false;
+}
+console.log(rangeCheck(1,2,3));
+console.log(rangeCheck(2,2,3));
