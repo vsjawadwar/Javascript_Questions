49fc8a30b4fd6636d8a28fb757e1495d06614560
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-10-11 23:26:32 +0530

    Matrix problem has been initiated

---
+++ Basic_1/121_Matrix.js
@@ -0,0 +1,2 @@
+/*Write a JavaScript program to check whether a given matrix is lower triangular or not. 
+Note: A square matrix is called lower triangular if all the entries above the main diagonal are zero.*/
