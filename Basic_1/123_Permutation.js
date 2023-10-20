ba99d94aa604df366ab775c5617f3ac8d0cb13c1
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-10-20 23:33:21 +0530

    Permutation problem has been solved

---
+++ Basic_1/123_Permutation.js
@@ -0,0 +1,12 @@
+// Write a JavaScript program to find out if the members of a given array of integers are a permutation of numbers from 1 to a given integer. 
+
+
+function is_permutation(input_arr, n) {
+    for (var i = 0; i < n; i++) {
+        if (input_arr.indexOf(i + 1) < 0) 
+          return false;
+    }
+    return true;
+}
+console.log(is_permutation([1, 2, 3, 4, 5], 5));
+console.log(is_permutation([1, 2, 3, 5], 5));
