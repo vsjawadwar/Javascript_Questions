1e91f3a52f9c420138d22b7bae6fcbe60e48de62
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-10-26 11:32:24 +0530

    Same digit check problem has been solved by another method

--- Basic_1/140_SameDigitNumber.js
+++ Basic_1/140_SameDigitNumber.js
@@ -1,10 +1,21 @@
 // Write a JavaScript program to check whether all the digits in a given number are the same or not.
 function sameDigitNumber(num){
+/*
+Method 1: 
     let arr=num.toString().split('')
     for(let i=0;i<arr.length;i++){
         if(arr[0]!= arr[i]){
             return false;
         }
+    }
+    return true;
+Method 2:
+*/
+
+let digit=num % 10;
+    while(num){
+        if(num % 10 !== digit) return false;
+        num=Math.floor(num / 10);
     }
     return true;
 }
