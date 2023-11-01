e2b7023ba13db2ac95a448d8fbe97161bea62521
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-11-01 23:43:31 +0530

    ForInForEach Basics

---
+++ ES6_Part_1/ForInBasics.js
@@ -0,0 +1,23 @@
+const numArray=[10,12,14,16,18,20];
+for (const key in numArray) {
+    // console.log(numArray[key]);
+}
+
+for (const key of numArray) {
+    console.log(key);
+    
+}
+
+const str="Jay Shree Ram";
+for (const val of str) {
+    // console.log(val);
+}
+
+const map=new Map();
+map.set('IN','India');
+map.set('USA','United States Of America');
+map.set('Fr','France');
+
+// for (const iterator of object) {
+    
+// }
