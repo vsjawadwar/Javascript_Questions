ce91a479f7da5314c6fac7d1fc1157ca84d1bf77
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-11-04 23:46:33 +0530

    Filter method implemented

---
+++ ES6_Part_1/8_Filter.js
@@ -0,0 +1,14 @@
+// Write a JavaScript program to filter out the specified values from a specified array. Return the original array without filtered values.
+//#Source https://bit.ly/2neWfJ2 
+const pull = (arr, ...args) => {
+    let argState = Array.isArray(args[0]) ? args[0] : args;
+    let pulled = arr.filter((v, i) => !argState.includes(v));
+    arr.length = 0;
+    pulled.forEach(v => arr.push(v));
+    return pulled;
+  };
+  let arra1 = ['a', 'b', 'c', 'a', 'b', 'c'];
+  console.log(pull(arra1, 'a', 'c'));
+  let arra2 =  ['a', 'b', 'c', 'a', 'b', 'c'];
+  console.log(pull(arra2, 'b'));
+  
