cee85e00c49a816e8f4bbc8700fd16c9ee722c35
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-11-08 23:45:07 +0530

    Reduce demo problem has been solved

---
+++ ES6_Part_1/15_ReduceDemo.js
@@ -0,0 +1,12 @@
+/*
+Write a JavaScript program to return the minimum-maximum value of an array, after applying the provided function to set a comparing rule.
+
+Use Array.prototype.reduce() in combination with the comparator function to get the appropriate element in the array.
+Omit the second argument, comparator, to use the default one that returns the minimum element in the array. */
+const reduce_Which = (arr, comparator = (a, b) => a - b) =>
+  arr.reduce((a, b) => (comparator(a, b) >= 0 ? b : a));
+console.log(reduce_Which([1, 3, 2])); 
+console.log(reduce_Which([10, 30, 20], (a, b) => b - a));  
+console.log(reduce_Which(
+  [{ name: 'Kevin', age: 16 }, { name: 'John', age: 20 }, { name: 'Ani', age: 19 }],
+  (a, b) => a.age - b.age)); 
