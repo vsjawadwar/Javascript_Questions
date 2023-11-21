c677db4eecbd05c6d88a0a957ef8ca30f040c7f9
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-11-21 23:49:19 +0530

    Comparison problem has been solved

---
+++ ES6_Part_1/47_Comparison.js
@@ -0,0 +1,11 @@
+// Write a JavaScript program to perform a deep comparison between two values to determine if they are equivalent.
+
+function compare(a,b){
+    if(a === b){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(compare(0,'10'));
+
