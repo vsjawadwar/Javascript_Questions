bd3e5db86c07fc321fd978e2e0d85cc8c7ed527b
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-12-17 00:10:23 +0530

    Slider login project has been completed

--- ES6_Part_1/SliderLogin/Script.js
+++ ES6_Part_1/SliderLogin/Script.js
@@ -3,9 +3,11 @@ const signInButton=document.getElementById('sign-in');
 const container=document.getElementById('container');
 
 signUpButton.addEventListener('click',function(e){
+    // e.preventDefault();
     container.classList.add("right-panel-active");
 },false);
 
 signInButton.addEveneListener('click',function(){
+    // e.preventDefault();
     container.classList.remove("right-panel-active");
 },false);
